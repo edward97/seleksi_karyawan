34789dc1191dc562a8bb143197b767e3a9a01d88
edward97 <edw.suryajaya@gmail.com>
2018-07-15 19:27:46 +0700

    dashboard admin

---
+++ application/views/admin/v_dashboard.php
@@ -0,0 +1,135 @@
+<div class="content">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="card">
+					<div class="card-header bg-light">
+						<div class="row">
+							<div class="col-md-6">
+								<p>Dashboard Session</p>
+							</div>
+
+							<div class="col-md-6 text-right">
+								<button type="button" class="btn btn-primary bg-info">
+									Profile <span class="badge badge-danger">9</span>
+								</button>
+							</div>
+
+							<div class="col-md-6">
+								<p>Terdapat 2 sesi aktif saat ini</p>
+							</div>
+						</div>
+
+					</div>
+
+					<div class="card-body">
+						<div class="card">
+							<div class="card-header bg-light">
+								<div class="row">
+									<div class="col-md-6">
+										<a class="card-link" data-toggle="collapse" href="#collapse1">
+											Session Akuntansi
+										</a>
+									</div>
+
+									<div class="col-md-6 text-right">
+										<a href="#" class="btn btn-warning">Edit Session</a>
+										<a href="#" class="btn btn-success">Close Session</a>
+									</div>
+								</div>
+							</div>
+
+							<div id="collapse1" class="collapse show">
+								<div class="card-body">
+									<div class="row">
+										<div class="col-md-12">
+											<p class="text-success">Tahapan Registrasi</p>
+										</div>
+
+										<div class="col-md-6">
+											<a href="#" class="btn btn-outline-secondary text-danger">250</a> orang telah melakukan registrasi
+										</div>
+
+										<div class="col-md-6 text-right">
+											<a href="#" class="btn btn-outline-secondary">Lihat Detail</a>
+										</div>
+									</div>
+									<hr>
+
+									<div class="row">
+										<div class="col-md-12">
+											<p class="text-success">Tahapan Ujian Online</p>
+										</div>
+
+										<div class="col-md-6">
+											<a href="#" class="btn btn-outline-secondary text-danger">250</a> orang telah melakukan ujian online
+										</div>
+
+										<div class="col-md-6 text-right">
+											<a href="#" class="btn btn-outline-secondary">Lihat Detail</a>
+										</div>
+									</div>
+									<hr>
+
+									<div class="row">
+										<div class="col-md-12">
+											<p class="text-success">Tahapan Ujian Langsung</p>
+										</div>
+
+										<div class="col-md-6">
+											<a href="#" class="btn btn-outline-secondary text-danger">250</a> orang telah melakukan Langsung
+										</div>
+
+										<div class="col-md-6 text-right">
+											<a href="#" class="btn btn-outline-secondary">Lihat Detail</a>
+										</div>
+									</div>
+									<hr>
+
+									<div class="row">
+										<div class="col-md-12">
+											<p class="text-success">Tahapan Interview</p>
+										</div>
+
+										<div class="col-md-6">
+											<a href="#" class="btn btn-outline-secondary text-danger">250</a> orang telah melakukan interview
+										</div>
+
+										<div class="col-md-6 text-right">
+											<a href="#" class="btn btn-outline-secondary">Lihat Detail</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="card">
+							<div class="card-header bg-light">
+								<div class="row">
+									<div class="col-md-6">
+										<a class="card-link" data-toggle="collapse" href="#collapse2">
+											Session IT
+										</a>
+									</div>
+
+									<div class="col-md-6 text-right">
+										<a href="#" class="btn btn-warning">Edit Session</a>
+										<a href="#" class="btn btn-success">Close Session</a>
+									</div>
+								</div>
+							</div>
+
+							<div id="collapse2" class="collapse show">
+								<div class="card-body">
+									Lorem ipsum..
+								</div>
+							</div>
+						</div>
+
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
